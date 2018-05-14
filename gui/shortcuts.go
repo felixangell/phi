df380132b37904c8be75b561b1e7ce7c3705463d
Felix Angell <felixangell@members.fsf.org>
2018-05-14 20:27:19 +0100

    atomic file saving, closes #27

--- gui/shortcuts.go
+++ gui/shortcuts.go
@@ -2,8 +2,12 @@ package gui
 
 import (
 	"bytes"
+	"crypto/rand"
+	"encoding/hex"
 	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/atotto/clipboard"
 )
@@ -33,6 +37,12 @@ func Paste(v *View, commands []string) bool {
 	return false
 }
 
+func genFileName(dir, prefix, suffix string) string {
+	randBytes := make([]byte, 16)
+	rand.Read(randBytes)
+	return filepath.Join(dir, prefix+hex.EncodeToString(randBytes)+suffix)
+}
+
 // NOTE: all shortcuts return a bool
 // this is whether or not they have
 // modified the buffer
@@ -40,6 +50,9 @@ func Paste(v *View, commands []string) bool {
 // re-rendered.
 
 func Save(v *View, commands []string) bool {
+	// TODO Config option for this.
+	atomicFileSave := true
+
 	b := v.getCurrentBuff()
 	if b == nil {
 		return false
@@ -70,13 +83,40 @@ func Save(v *View, commands []string) bool {
 	// and then move the file overwriting when the
 	// file has been written?
 
-	err := ioutil.WriteFile(b.filePath, buffer.Bytes(), 0775)
+	filePath := b.filePath
+	if atomicFileSave {
+		absPath, err := filepath.Abs(filePath)
+		if err != nil {
+			log.Println("cant get abs dir of path ", b.filePath, " can't save!")
+			return false
+		}
+
+		ext := filepath.Ext(absPath)
+		dir := filepath.Dir(absPath)
+
+		filePath = genFileName(dir, "", ext)
+	}
+
+	err := ioutil.WriteFile(filePath, buffer.Bytes(), 0775)
 	if err != nil {
 		log.Println(err.Error())
 	}
+
 	log.Println("Wrote file '" + b.filePath + "' to disk")
+	if atomicFileSave {
+		log.Println("- Wrote atomically to file:", filePath)
+	}
+
+	if atomicFileSave {
+		log.Println("- Over-writing atomic file save")
+
+		err := os.Rename(filePath, b.filePath)
+		if err != nil {
+			log.Println("Failed to save!", err.Error())
+			return false
+		}
+	}
 
 	b.modified = false
-
 	return false
 }
