2d75143db1bbb401a0bec05040d2ef775e1328cb
Felix Angell <felixangell@otzaf.org>
2021-01-09 12:35:55 +0000

    use go modules with latest version of strife

--- buff/buffer.go
+++ buff/buffer.go
@@ -2,6 +2,13 @@ package buff
 
 import (
 	"fmt"
+	"github.com/felixangell/phi/cfg"
+	"github.com/felixangell/phi/gui"
+	"github.com/felixangell/phi/lex"
+	"github.com/felixangell/phi/piecetable"
+	"github.com/felixangell/strife"
+	"github.com/lithammer/fuzzysearch/fuzzy"
+	"github.com/veandco/go-sdl2/sdl"
 	"io/ioutil"
 	"log"
 	"math"
@@ -11,14 +18,6 @@ import (
 	"strings"
 	"time"
 	"unicode"
-
-	"github.com/felixangell/phi/cfg"
-	"github.com/felixangell/phi/gui"
-	"github.com/felixangell/phi/lex"
-	"github.com/felixangell/phi/piecetable"
-	"github.com/felixangell/strife"
-	"github.com/lithammer/fuzzysearch/fuzzy"
-	"github.com/veandco/go-sdl2/sdl"
 )
 
 var (
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/felixangell/phi
+
+go 1.14
+
+require (
+	github.com/atotto/clipboard v0.1.2
+	github.com/felixangell/strife v0.2.0
+	github.com/fsnotify/fsnotify v1.4.9
+	github.com/lithammer/fuzzysearch v1.1.1
+	github.com/pelletier/go-toml v1.8.1
+	github.com/stretchr/testify v1.6.1
+	github.com/veandco/go-sdl2 v0.4.5
+	golang.org/x/sys v0.0.0-20210108172913-0df2131ae363 // indirect
+	golang.org/x/text v0.3.5 // indirect
+)
